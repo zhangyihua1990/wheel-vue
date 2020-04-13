5e09b617e1442d857edb1fd63f2bae06a494ba49
zack <zhangyihua90s@qq.com>
2020-04-13 21:39:51 +0800

    重构row和col组件

--- col.vue
+++ col.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-   :style="{paddingLeft: gutter/2 + 'px',paddingRight: gutter/2 + 'px'}"
+  <div class="col" :class="colClass"
+       :style="colStyle"
   >
     <div style="border: 1px solid green; height: 100px">
       <slot></slot>
@@ -17,6 +17,14 @@
       offset: {
         type: [Number, String],
       },
+    },
+    computed: {
+      colStyle() {
+        return {paddingLeft: this.gutter / 2 + 'px', paddingRight: this.gutter / 2 + 'px'};
+      },
+      colClass() {
+        return [this.span && `col-${this.span}`, this.offset && `offset-${this.offset}`]
+      }
     },
     data() {
       return {
--- row.vue
+++ row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px'}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -11,12 +11,16 @@
         type: [String, Number],
       },
     },
+    computed:{
+      rowStyle () {
+        return {marginLeft: -this.gutter/2 + 'px', marginRight: -this.gutter/2 + 'px'}
+      }
+    },
     mounted() {
       this.$children.forEach((vm) => {
         vm.gutter = this.gutter;
       });
     },
-
   };
 </script>
 
