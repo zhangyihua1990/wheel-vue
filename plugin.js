c53251bfe643284ecf6ab1ef87b699e70e7be5f4
zack <zhangyihua90s@qq.com>
2020-04-19 23:26:17 +0800

    解构赋值重构

--- plugin.js
+++ plugin.js
@@ -1,13 +1,17 @@
 import Toast from './toast';
 
+function creatToast({Vue, message, propsData}) {
+  let Constructor = Vue.extend(Toast);
+  let toast = new Constructor({propsData});
+  toast.$slots.default = [message];
+  toast.$mount();
+  document.body.appendChild(toast.$el);
+}
+
 export default {
   install(Vue, options) {
-    Vue.prototype.$toast = function (message,options) {
-      let Constructor = Vue.extend(Toast);
-      let toast = new Constructor(options);
-      toast.$slots.default = [message];
-      toast.$mount();
-      document.body.appendChild(toast.$el);
+    Vue.prototype.$toast = function (message, options) {
+      creatToast({Vue, message, propsData:options});
     };
   },
 };
